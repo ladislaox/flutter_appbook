930aa669dc542226aa7bce4432678d938d97d06f
ladislao <ladislaox@gmail.com>
2024-01-02 20:21:28 -0500

    booklist.dart - uso de cards y creacion de widgets

--- lib/page/booklist.dart
+++ lib/page/booklist.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+
 
 class Booklist extends StatelessWidget {
   const Booklist({super.key});
@@ -7,9 +10,70 @@ class Booklist extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Listado de libros')),
-      body: Container(
-        child: Text('Libros de literatura'),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2
+        ),
+        children: [
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+          _bookCard(),
+        ],
       ),
     );
   }
+
+  Widget _bookCard() {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.only(top: 8.0, left: 8.0,right: 8.0),
+        height: 150.0,
+        width: 150.0,
+        child: Column(
+          children: [
+            _bookCardImage(),
+            const SizedBox(height: 8),
+            _bookCardTitle(),
+            const SizedBox(height: 8),
+            _bookCardPrice(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _bookCardImage() {
+    return Container(
+      width: 80.0,
+      height: 80.0,
+      child: const Placeholder()
+    );
+  }
+
+  Widget _bookCardTitle() {
+    return Container(
+      child: const Text('El hombre mas rico de babilonia'),
+    );
+  }
+
+  Widget _bookCardPrice() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: const Text(
+        'S/90.00',
+        textAlign: TextAlign.start,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
 }
